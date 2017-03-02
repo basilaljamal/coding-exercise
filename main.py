1aef025c83f6dda6932cd15ed00bfea2eac01d69
basil <basil@localhost.localdomain>
2017-03-02 18:09:46 +0200

    main.py updated

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 from bottle import Bottle, response, run, request
-import urllib
 import json
 import logging
 import requests
 
 app = Bottle()
-
-
 
 @app.error(500)
 @app.error(404)
@@ -24,28 +21,16 @@ def my_error(error):
     logging.error(r)
     return r
 
-logging.info("Just Started :)")
-
-@app.get('/')
-def hello():
-    r = response
-    r.headers['Access-Control-Allow-Origin'] = '*'
-    r = "Hi :)"
-    return r
-
 
 @app.get('/getOffers')
 def getOffers():
-    #params =json.loads(request.body.read().decode())
     params = request.query_string
     BaseURL = 'https://offersvc.expedia.com/offers/v2/getOffers?'
-    RequestURL =BaseURL + params #urllib.urlencode(params)
-    print RequestURL
+    RequestURL =BaseURL + params
     r = response
     r.headers['Content-Type'] = 'application/json'
     r.headers['Access-Control-Allow-Origin'] = '*'
     r =requests.get(RequestURL).text
     return r
    
-
-run(app, host='0.0.0.0', debug=True)
+run(app, host='0.0.0.0')
